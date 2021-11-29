154a8fd466ae2ba32ba25d3d862b5cf1a548c85a
121Anastasia121 <86976844+121Anastasia121@users.noreply.github.com>
2021-11-29 03:24:31 +0300

    Homework_10

---
+++ index.js
@@ -0,0 +1,61 @@
+// 2.......??????
+
+// const arr = [2, 4, 1, 2, 4, 10, 6, 8, 9];
+// const home = [];
+
+// function CountIdentic(arr) {
+//   let result = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let h = 0; h < arr.length; h++) {
+//       if (arr[i] !== arr[h]) {
+//        const home=(false); 
+//       }  else {
+//         result++;
+//       }    
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(CountIdentic(arr));
+
+
+// 1......
+
+// const range = "55639217";
+
+// const myfunction = (range)=> {
+//   const first = range.split ("");
+//   const second = [];
+//   for (let i = 0; i < first.length; i++) {
+//     second.push(first[i]);
+//     if (first[i] % 2 && first[i+1] % 2) {
+//       second.push(":");
+//     }
+//   }
+//   return second.join("");
+// } 
+
+// console.log(myfunction(range));
+
+
+
+// 3.......
+
+// const array = [1, 2, 3, 2, 4, 5];
+
+// const unArray = [... new Set(array)];
+
+// console.log(unArray);
+
+
+//4.........
+
+// const number = [1, 2, 10, 2, 5, 1, 8, 13, 3];
+
+// const removeItem = (arr, num) => {
+//   arr.shift(num);
+//   return arr;
+// }
+
+// console.log(removeItem(number));
